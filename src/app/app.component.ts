87b655e4527ab69d87efa3464947cfa039088309
Venkatachalapathi <venkatachalapathi@Venkat.local>
2019-01-19 15:37:04 +0530

    fixed the footer section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  showMenu = true;
-  darkModeActive: boolean;
-
-  constructor() {}
-
-  ngOnInit() {}
-
-  toggleMenu() {
-    this.showMenu = !this.showMenu;
+  ngOnInit(): void {
   }
-
-  modeToggleSwitch() {}
 }
